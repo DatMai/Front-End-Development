49577b5cb72ae348763fc419bbe3fef7bf718c51
moddbno <18130092@st.hcmuaf.edu.vn>
2021-07-30 09:37:20 +0700

    fix-1

--- appchat/src/app/app.component.spec.ts
+++ appchat/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { RightContainerComponent } from './main-screen/right-container/right-container.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
