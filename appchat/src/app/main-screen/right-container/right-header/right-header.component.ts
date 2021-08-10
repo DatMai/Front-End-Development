3df0e1d9e1124360e06f50c774bfb9cdb3a3d52a
moddbno <18130092@st.hcmuaf.edu.vn>
2021-08-10 18:54:08 +0700

    effect

--- appchat/src/app/main-screen/right-container/right-header/right-header.component.ts
+++ appchat/src/app/main-screen/right-container/right-header/right-header.component.ts
@@ -1,10 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/service/data.service';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-right-header',
   templateUrl: './right-header.component.html',
-  styleUrls: ['./right-header.component.scss']
+  styleUrls: ['./right-header.component.scss'],
+  animations:[
+    trigger('headerAnimation',[
+      transition(':enter',[
+        style({
+        opacity:0,
+        transform :'translateX(-1vw)'
+      }),
+        animate("0.3s",style({
+          opacity:1,
+          transform:'translateX(0)'
+        }
+        )
+        )]),
+      transition(':leave',[
+        animate("0.3s",style({
+          opacity:0,
+          transform:'translateX(1vw)'
+        }))])
+    ]),
+    trigger('iconAnimation',[
+      transition(':enter',[
+        style({
+          opacity:0,
+          transform: 'translateX(1vw)'
+        }),
+        animate("0.3s",style({
+            opacity:1,
+            transform:'translateX(0)'
+          }
+          )
+        )
+      ])
+    ])
+  ]
 })
 export class RightHeaderComponent implements OnInit {
 
@@ -19,5 +54,5 @@ export class RightHeaderComponent implements OnInit {
   public showManager(){
     this.dataService.isShowManager=!this.dataService.isShowManager;
   }
-  
+
 }
--- appchat/src/app/main-screen/right-container/right-manager/right-manager.component.ts
+++ appchat/src/app/main-screen/right-container/right-manager/right-manager.component.ts
@@ -1,11 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DataService } from 'src/app/service/data.service';
+import {animate, keyframes, query, stagger, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-right-manager',
   templateUrl: './right-manager.component.html',
-  styleUrls: ['./right-manager.component.scss']
+  styleUrls: ['./right-manager.component.scss'],
+  // animations: [
+  //   trigger('listAnimation',[
+  //     state('hide',style({
+  //       opacity:0,
+  //       transform:"translateX(30px)",
+  //       height:"0px"
+  //
+  //     })),
+  //     transition('hide => show',[
+  //       animate('0.3s',style({
+  //         opacity:1,
+  //         transform:"translateX(0px)",
+  //         height:"fit-content",
+  //       }))
+  //     ]),
+  //     transition('show => hide',[
+  //       animate('0.3s',style({
+  //         opacity:0,
+  //         transform:"translateX(30px)",
+  //         height:"0px",
+  //       }))
+  //     ])
+  //   ])
+  // ]
+  animations:[
+    trigger("listAnimation",[
+
+      transition(':enter',[
+        query('.list__item', style({opacity:0}),{optional:true}),
+        query('.list__item', stagger('50ms',[
+          animate('0.3s', keyframes([
+            style({opacity:0, transform: "translateX(75px)",offset: 0}),
+            style({opacity:1, transform: "translateX(0px)",offset: 1}),
+          ]))
+        ]),{optional:true})
+      ]),
+      transition(':leave',[
+        query('.list__item', style({opacity:1}),{optional:true}),
+        query('.list__item', stagger('50ms',[
+          animate('0.3s', keyframes([
+            style({opacity:1, transform: "translateX(0px)",offset: 0}),
+            style({opacity:0, transform: "translateX(75px)",offset: 1}),
+          ]))
+        ]),{optional:true})
+      ])
+    ]),
+    ],
 })
 export class RightManagerComponent implements OnInit {
   isShowChatSetting:boolean = false;
@@ -16,6 +64,10 @@ export class RightManagerComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+ public listState(flag:boolean){
+    return flag? 'show':'hide';
   }
 
   public isActive(bol :boolean){
